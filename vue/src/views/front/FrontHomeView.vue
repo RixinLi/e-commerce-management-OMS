9aaf2f7c494f0971d7b0d545c10ae86c4124956b
Rixin Li <90631474+RixinLi@users.noreply.github.com>
2025-05-21 13:31:02 -0400

    前端基本布局页面展示

--- vue/src/views/front/FrontHomeView.vue
+++ vue/src/views/front/FrontHomeView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cea4d2130f6ebaea10986e89545b16d6700503cc8e101aa743db59dee22e1485
-size 1206
+oid sha256:49b23448169c07a4dc32e080e1a01f7fe2c4eb92b7fbfd03e538c628137de25a
+size 3192
