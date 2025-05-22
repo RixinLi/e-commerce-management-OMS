6012895179c2047586e2d2fee1d53410be0a28df
Rixin Li <90631474+RixinLi@users.noreply.github.com>
2025-05-22 09:41:51 -0400

    SSL证书配置尝试

--- vue/src/views/LoginView.vue
+++ vue/src/views/LoginView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1082ec6a68c7a5684863b6d15f6e927adaa21b632c86ffd897f6fa0bf64a9d5
-size 5844
+oid sha256:50a474877ae590b0502eb36e0014dfb10d307a6cef8d24c84ef54596dae5f3e6
+size 5850
