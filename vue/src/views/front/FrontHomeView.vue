2214f91ca62f76cebcb7520f4a29beb2409f09b7
Rixin Li <90631474+RixinLi@users.noreply.github.com>
2025-05-22 21:37:39 -0400

    静态团购走马布局设计（未完成）

--- vue/src/views/front/FrontHomeView.vue
+++ vue/src/views/front/FrontHomeView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c617a8425d03ab3772e28e4326797b0a72c30dc6c0866feb9934fb84e3825b81
-size 5597
+oid sha256:a9679677eafe3414d379ef92b7662fe17febbb0bf9091c53916369b5fd348857
+size 8244
